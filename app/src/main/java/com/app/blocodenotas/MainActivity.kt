1d11f69ff804929c1d8208f1d3556be4a5b8c795
55219 <rodolfo.silva.belo@gmail.com>
2021-03-14 21:10:42 -0300

    corrections

--- app/src/main/java/com/app/blocodenotas/MainActivity.kt
+++ app/src/main/java/com/app/blocodenotas/MainActivity.kt
@@ -6,17 +6,38 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.app.blocodenotas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val preferencia = PreferenciaAnotacao(applicationContext)
+        val botacaoCalcular = binding.fab
 
+        botacaoCalcular.setOnClickListener {
 
+            val anotacaoRecuperado = binding.editConteiner.anotacao.text.toString()
 
+            if (anotacaoRecuperado == ""){
+                Toast.makeText(this,"Escreva algo...", Toast.LENGTH_SHORT).show()
+            }else {
+                preferencia.SalvarAnotacao(anotacaoRecuperado)
+                Toast.makeText(this,"Anotação Salva", Toast.LENGTH_SHORT).show()
+            }
+        }
 
+        val anotacao = preferencia.RecuperarAnotacao()
+
+        if (anotacao != ""){
+            binding.editConteiner.anotacao.setText(anotacao)
+        }
 
     }
 
